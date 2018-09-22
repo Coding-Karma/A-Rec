52ec2b8071e22d6f9692158b8d16f1c7d49adf2c
Coding-Karma <saubhagyasrivastava339@gmail.com>
2018-09-23 04:19:26 +0530

    Updated Runme.sh

--- runme.sh
+++ runme.sh
@@ -1,3 +1,8 @@
+git clone https://github.com/1N3/Sn1per /opt/Sniper
+bash /opt/Sniper/build.sh
+bash /opt/Sniper/install.sh
+apt-get install inspy
+apt-get install wig
 apt-get install figlet
 git clone https://github.com/leapsecurity/InSpy /opt/wordlists
 git clone https://github.com/1N3/BlackWidow /opt/Blackwidow
